cf3d4599235d4683df5d198f31ee8c5dd3ea86d3
Nelson Chidi (Nelmatrix743) <nelmatrix155@gmail.com>
2025-11-17 16:12:21 +0000

    nevermind: nevermind

--- script.js
+++ script.js
@@ -2,4 +2,6 @@
  *  SOURCE CODE VERSION: 1.0.0
  *  PROGRAMMER: NELSON CHIDI (NELMATRIX)
  *  DATE-TIME WRITTEN: NOV 11, 2025; 3:51PM
+ *  REPOSITORY: https://github.com/NelMatrix743/qr_code_generator_web_app.git
  */
+
