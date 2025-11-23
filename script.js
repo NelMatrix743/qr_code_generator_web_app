e401171db8dce9e45b16e8c375f67df418d926ea
Nelson Chidi (Nelmatrix743) <nelmatrix155@gmail.com>
2025-11-23 19:29:44 +0000

    feat(logic): map core ui elements to javascript variables; define core api url

--- script.js
+++ script.js
@@ -5,3 +5,24 @@
  *  REPOSITORY: https://github.com/NelMatrix743/qr_code_generator_web_app.git
  */
 
+
+let inputContent = document.querySelector(".input-fld");
+let generateBtn = document.querySelector(".generate-btn");
+let downloadBtn = document.querySelector(".download-btn");
+let qrCodeImage = document.querySelector("#qr-display");
+
+
+let BASE_API_URL = "https://api.qrserver.com/v1/create-qr-code/"
+
+
+async function generateQRCode(qrcode_size, qrcode_data){
+    let QR_SIZE_PARAMETER = "?size=" + qrcode_size;
+    let QR_DATA_PARAMETER = "&data=" + qrcode_data;
+    let FULL_API_URL = BASE_API_URL + QR_SIZE_PARAMETER + QR_DATA_PARAMETER;
+    const response = await fetch(FULL_API_URL);
+    const qrCodeBlob = await response.blob();
+    const objectURL = URL.createObjectURL(qrCodeBlob);
+    qrCodeImage.src = objectURL;
+    return qrCodeBlob;
+}
+
