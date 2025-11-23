e7df8fa6415044a7435f456394f7266d15fa204a
Nelson Chidi (Nelmatrix743) <nelmatrix155@gmail.com>
2025-11-23 23:22:09 +0000

    feat(logic): resolve bug found in API request logic

--- script.js
+++ script.js
@@ -6,10 +6,10 @@
  */
 
 
-let inputContent = document.querySelector(".input-fld");
+let inputContent = document.getElementById("user-input");
+let qrCodeImage = document.getElementById("qr-display");
 let generateBtn = document.querySelector(".generate-btn");
 let downloadBtn = document.querySelector(".download-btn");
-let qrCodeImage = document.querySelector("#qr-display");
 
 
 let BASE_API_URL = "https://api.qrserver.com/v1/create-qr-code/"
@@ -17,6 +17,7 @@ let BASE_API_URL = "https://api.qrserver.com/v1/create-qr-code/"
 
 async function generateQRCode(qrcode_size, qrcode_data){
     let FULL_API_URL = BASE_API_URL + "?size=" + qrcode_size + "&data=" + qrcode_data;
+    console.log(FULL_API_URL);
     const response = await fetch(FULL_API_URL);
     return await response.blob();
 }
@@ -43,3 +44,15 @@ async function downloadQRCode(qrcode_blob){
     } 
 }
 
+
+// main entry point
+let QR_CODE_SIZE = "150x150";
+let QR_CODE_DATA = "";
+
+
+// implment event listeners
+generateBtn.addEventListener("click", async () => {
+    QR_CODE_DATA = encodeURIComponent(inputContent.value);
+    let qrCodeBlobResult = await generateQRCode(QR_CODE_SIZE, QR_CODE_DATA);
+    qrCodeImage.src = URL.createObjectURL(qrCodeBlobResult);
+});
